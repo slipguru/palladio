6f5c02651c21bb7349ba53987f8b91144bba87b6
Matteo Barbieri <matteo.barbieri@dibris.unige.it>
2016-07-09 14:50:20 +0200

    Removed the import palladio statement (it kinda makes no sense in this script)

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import argparse
 
-from palladio import __version__
+#from palladio import __version__
 
 
 if __name__ == '__main__':
@@ -14,8 +14,9 @@ if __name__ == '__main__':
     parser.add_argument('deployment_folder', nargs='?',
                         default=os.path.expanduser("~"),
                         help="Specify the deployment folder")
-    parser.add_argument('--version', action='version',
-                        version='%(prog)s v'+__version__)
+    #parser.add_argument('--version', action='version',
+    #                    version='%(prog)s v'+__version__)
+
     parser.add_argument("-s", "--sample-data",
                         action="store_true", dest="sample_data", default=False,
                         help="Also copy sample data to the deployment folder")
