0e9bbfc915bac19a15881d5d1ed72fec2e490f0b
Matteo Barbieri <matteo.barbieri@dibris.unige.it>
2016-07-12 17:48:28 +0200

    completed DatasetNPY class for datasets stored in npy files

---
+++ palladio/datasets/DatasetNPY.py
@@ -0,0 +1,155 @@
+import os
+
+import numpy as np
+
+from .Dataset import Dataset
+
+class DatasetNPY(Dataset):
+    """Dataset composed by data matrix and labels vector, stored in two csv files.
+    
+    
+    """
+    
+    def load_dataset(self, base_path):
+        """Read data matrix and labels vector from files.
+        
+        Requires a .pkl file containing a dictionary storing
+        samples and features names (keys ``index`` and
+        ``columns`` respectively).
+        
+        Parameters
+        ----------
+        
+        base_path : string
+            The base path relative to which files are stored.
+        
+        Returns
+        -------
+        
+        data : ndarray
+            The :math:`n \\times p` data matrix .
+        
+        labels : ndarray
+            The :math:`n`-dimensional vector containing labels.
+            
+        feature_names : list
+            The list containing the names of the features
+        
+        """
+        
+        data_path = os.path.join(base_path, self.get_file('data'))
+        labels_path = os.path.join(base_path, self.get_file('labels'))
+        indcol_path = os.path.join(base_path, self.get_file('indcol'))
+        
+        #####################
+        ### DATA + LABELS ###
+        #####################
+        data = np.load(data_path) 
+        labels = np.load(labels_path) 
+     
+        if self.get_option('samples_on') == 'col': 
+            data = data.T 
+     
+        ### TODO FIX IF DATA IS TRANSPOSED??? 
+        with open(indcol_path, 'r') as f: 
+            res = pkl.load(f) 
+     
+            feature_names = np.array(res['columns'] )
+            samples_names = np.array(res['index'])
+            
+        # if not self.get_option('data_preprocessing') is None:
+        # ### TODO Check!!!
+        #     # if rank == 0:
+        #         # print("Preprocessing data...")
+        #         
+        #     self.get_option('data_preprocessing').load_data(pd_data)
+        #     pd_data = self.get_option('data_preprocessing').process()
+        
+        #######################
+        ### LABELS TO -1/+1 ###
+        #######################
+        
+        if not self.get_option('positive_label') is None:
+            poslab = self.get_option('positive_label')
+        else:
+            uv = np.sort(np.unique(pd_labels.values))
+    
+            if len(uv) != 2:
+                raise Exception("More than two unique values in the labels array")
+    
+            poslab = uv[0]
+        
+        ### Auxiliary function required to convert the labels to
+        ### -1/+1
+        def _toPlusMinus(x):
+            """Converts the values in the labels"""
+            if x == poslab:
+                return +1.0
+            else:
+                return -1.0
+        
+        labels_mapped = map(_toPlusMinus, labels) 
+        labels = np.array(labels_mapped) 
+        
+        return data, labels, feature_names
+
+
+def load_data_npy_pkl(config_path, config, data_path, labels_path, indcol_path = None): 
+    """Load data, labels and variables names. 
+ 
+    Parameters 
+    ---------- 
+    config_path : string 
+        A path to the configuration file containing all required information 
+        to run a **PALLADIO** session. 
+ 
+    config : object 
+        The object containing all configuration parameters for the session. 
+ 
+    data_path : string 
+        A path to the ``.npy`` data file. 
+ 
+    labels_path : string 
+        A path to the ``.npy`` labels file. 
+ 
+    indcol_path : string, optional 
+        A path to the ``.pkl`` file containing sample and column names. 
+    """ 
+    # Configuration File 
+    config_dir = os.path.dirname(config_path) 
+ 
+    if rank == 0: 
+        print("") #-------------------------------------------------------------------- 
+        print('Reading data... ') 
+ 
+ 
+    
+ 
+ 
+    ### TODO FIX 
+    # if not config.data_preprocessing is None: 
+    #     if rank == 0: 
+    #         print("Preprocessing data...") 
+    #     config.data_preprocessing.load_data(pd_data) 
+    #     pd_data = config.data_preprocessing.process() 
+ 
+    if not config.positive_label is None: 
+        poslab = config.positive_label 
+    else: 
+        uv = np.sort(np.unique(labels)) 
+ 
+        if len(uv) != 2: 
+            raise Exception("More than two unique values in the labels array") 
+ 
+        poslab = uv[0] 
+ 
+    def _toPlusMinus(x): 
+        """Converts the values in the labels""" 
+        if x == poslab: 
+            return +1.0 
+        else: 
+            return -1.0 
+ 
+    
+ 
+    return data, labels, probeset_names 
