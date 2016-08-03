bug fixed: in extremely data poor circumstances it may happen that in your test set you only have, let's say, 2 samples. In these circmustances precision and recall (or any other score) may be NaN since it is calculated dividing by 0. So now instead of using np.mean and np.std for stats report, I'm using np.nanmean and np.nanstd. However, you realize that there is something fishy when you see that your std is = 0.

palladio/plotting.py
@@ -91,11 +91,11 @@ def distributions(v_regular, v_permutation, base_folder, metric):
         # hist_kws : {'alpha' : 0.8}
     }
 
-    reg_mean = np.mean(v_regular)
-    reg_std = np.std(v_regular)
+    reg_mean = np.nanmean(v_regular)
+    reg_std = np.nanstd(v_regular)
 
-    perm_mean = np.mean(v_permutation)
-    perm_std = np.std(v_permutation)
+    perm_mean = np.nanmean(v_permutation)
+    perm_std = np.nanstd(v_permutation)
 
     sns.distplot(v_permutation*scale,
                  label = "Permutation tests \nMean = {0:.2f}, STD = {1:.2f}".format(perm_mean, perm_std),

scripts/pd_analysis.py
@@ -40,6 +40,13 @@ def analyze_experiment(exp_folder, config, poslab):
     TN = np.sum((Y_lr == -1) * (Y_ts == Y_lr))
     FN = np.sum((Y_lr == -1) * (Y_ts != Y_lr))
 
+    # print("----------------------------")
+    # print("TP: {}".format(TP))
+    # print("FP: {}".format(FP))
+    # print("TN: {}".format(TN))
+    # print("FN: {}".format(FN))
+    # print("----------------------------")
+
     accuracy = (TP + TN) / float(TP + FP + FN + TN)
     balanced_accuracy = 0.5 * ( (TP / float(TP + FN)) + (TN / float(TN + FP)) )
 
@@ -47,7 +54,7 @@ def analyze_experiment(exp_folder, config, poslab):
     MCC = ( ((TP * TN) - (FP * FN)) / (1.0 if den == 0 else np.sqrt(den)) )
 
     if poslab is not None:
-        precision =  TP / float(TP + FP)
+        precision = TP / float(TP + FP)
         recall = TP / float(TP + FN)
         F1 = 2.0 * ((precision * recall) / (precision + recall))
     else:
@@ -94,7 +101,7 @@ def analyze_experiments(base_folder, config):
 
     experiments_folder = os.path.join(base_folder, 'experiments')
 
-    data, labels, feature_names  = dataset.load_dataset(base_folder)
+    data, labels, feature_names = dataset.load_dataset(base_folder)
 
     feature_names = np.array(feature_names)
 